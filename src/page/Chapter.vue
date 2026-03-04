e6cbb3fe12b96bc0156582c3e5d0d0357dbca809
Roger <Roger0122>
2026-03-04 16:48:27 +0800

    fix: 文字樣式

--- src/page/Chapter.vue
+++ src/page/Chapter.vue
@@ -3,7 +3,7 @@
     <details
       v-for="chapter in chapters"
       :key="chapter.id"
-      class="bg-gray-800 rounded-xl shadow-lg p-6"
+      class="bg-white rounded-xl shadow-lg p-6"
     >
       <summary class="cursor-pointer font-bold text-lg">
         {{ chapter.title }}
@@ -14,9 +14,9 @@
           <span class="absolute -left-2 w-3 h-3 bg-yellow-400 rounded-full"></span>
 
           <div>
-            <span class="font-semibold">{{ task.area }}</span>
+            <span class="font-semibold text-2xl">{{ task.area }}</span>
 
-            <div class="text-sm text-gray-400">
+            <div class="text-sm text-amber-700">
               {{ task.content }}
             </div>
           </div>
